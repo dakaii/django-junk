5b4a85eef1b8c91e1e1016107ed2ed8c4be183c3
dakaii <daiki815@gmail.com>
2016-03-07 21:04:00 +0900

    initial

---
+++ chime/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Lesson(models.Model):
+	lesson_name = models.CharField(max_length=100)
+	description = models.TextField(max_length=200)
+	lesson_detail = models.TextField(max_length=200)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	created = models.DateTimeField(auto_now_add=True)
+	class Meta:
+		ordering = ('created',)
