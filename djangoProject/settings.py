444c9ef42f052b96a8a30d07bc88ed24e78dda17
dakaii <daiki815@gmail.com>
2016-03-11 22:00:10 +0900

    im confused

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -154,6 +154,5 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        'UNICODE_JSON': True
     ]
 }
