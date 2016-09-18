14b957a6238f2b4a27ac6339cab57831e687b1aa
dakaii <daiki815@gmail.com>
2016-09-18 16:19:10 +0900

    a

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.settings")
 SECRET_KEY = "^ab3n9-*_87@zr=yu2uo!h6+bp(=x#q&h6r0og5@f(_20uov=c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
